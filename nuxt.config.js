8f0db014d634e1f45705204214830a0bf742ae69
Bonfire <baranb@knights.ucf.edu>
2021-02-10 23:18:28 -0500

    feat: 🎸 add Navbar component

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
 
   router: {
-    base: '/<repository-name>/',
+    base: '/tf2autobot-config/',
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
