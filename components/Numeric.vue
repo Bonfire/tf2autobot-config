393b4c97dac3c9007dc976152aabb907efc21e74
Bonfire <baranb@knights.ucf.edu>
2021-02-18 20:26:17 -0500

    style: modify styling on input and numeric components

--- components/Numeric.vue
+++ components/Numeric.vue
@@ -10,7 +10,7 @@
         <input
           :id="id"
           type="number"
-          class="inline-flex justify-center w-auto rounded-md border border-gray-500 shadow-sm px-4 py-1 mt-px mb-px ml-4 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+          class="inline-flex justify-center w-auto rounded-md border border-gray-500 shadow-sm px-4 py-1 mt-px mb-2 ml-4 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
           :step="step"
           min="0"
           :value="value"
