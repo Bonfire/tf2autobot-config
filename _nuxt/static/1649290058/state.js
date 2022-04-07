4ea449c6f8efd09f47396a3e40c927f1ba386da5
Bonfire <Bonfire@users.noreply.github.com>
2022-04-07 00:07:39 +0000

    deploy: f876cea4a11266de82a8a7496d35176c187aedb3

---
+++ _nuxt/static/1649290058/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftf2autobot-config\u002F_nuxt\u002Fstatic\u002F1649290058",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftf2autobot-config\u002F",assetsPath:"\u002Ftf2autobot-config\u002F_nuxt\u002F",cdnURL:a}}}}(null));
